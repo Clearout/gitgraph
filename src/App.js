d1830b49cf8e33d93b48ad7a0b96f39dd69ab012
Joacim Jakobsen <joacim.jakobsen@boost.ai>
2018-10-19 20:39:05 +0200

    init git graph with example.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'gitgraph.js';
 
-class App extends Component {
+export default class App extends Component {
+  componentDidMount() {
+    this.gitGraph = getGitGraph();
+    this.master();
+  }
+  master = () => {
+    const master = this.gitGraph
+      .branch('master')
+      .commit()
+      .commit();
+
+    this.gitGraph
+      .branch('dev')
+      .commit()
+      .commit()
+      .merge(master);
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <canvas id="gitGraph" />
       </div>
     );
   }
 }
 
-export default App;
+const getGitGraph = () => {
+  const gitGraph = window.GitGraph;
+  const gitgraph = new gitGraph({
+    elementId: 'gitGraph',
+    template: 'metro',
+    orientation: 'horizontal'
+  });
+  return gitgraph;
+};
